462217e9e7a5a36a00fe17cfafdd88360f72e23a
Nelson Ngo <82756769+Nelson-Ngo@users.noreply.github.com>
2022-09-09 01:24:30 +1000

    Skeleton Code

---
+++ sept-question-2/src/test/java/com/example/septquestion2/SeptQuestion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.septquestion2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptQuestion2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
