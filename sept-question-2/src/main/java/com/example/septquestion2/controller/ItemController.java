5fc282b4e394267848bb5896a3f4aa59d16d71a0
Nelson Ngo <82756769+Nelson-Ngo@users.noreply.github.com>
2022-09-09 14:50:30 +1000

    Controller Package

---
+++ sept-question-2/src/main/java/com/example/septquestion2/controller/ItemController.java
@@ -0,0 +1,77 @@
+package com.example.septquestion2.controller;
+
+import java.net.URI;
+
+import com.example.septquestion2.dao.ItemDAO;
+import com.example.septquestion2.model.Item;
+import com.example.septquestion2.model.Items;
+
+import org.apache.coyote.Response;
+import org.apache.el.stream.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping(path = "/item")
+public class ItemController {
+    @Autowired
+    private ItemDAO newItemDAO;
+
+    //Get by ID
+    @GetMapping(path="/item{id}")
+    public ResponseEntity<Item> getItemId(@PathVariable String id, @RequestBody Item item) {
+        Item finditem = newItemDAO.getItemID(id);
+
+        return ResponseEntity.ok().body(finditem);
+    }
+
+    //Get All
+    @RequestMapping(path = "/item")
+    public Items getAllItems() {
+        return newItemDAO.getAllItems();
+    }
+
+    //Add
+    @PostMapping(path = "/item")
+    public ResponseEntity<Item> addNewItem(
+            @RequestBody Item item
+    )
+            throws Exception
+    {
+
+        newItemDAO.addItem(item);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/item/{id}")
+                .buildAndExpand(item.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    //Update
+    @PutMapping(path = "/item")
+    public Items updateItem(@RequestBody Item item) {
+        newItemDAO.updateItems(item.getId(), item.getName(), item.getDesc(), item.getPrice());
+
+        return newItemDAO.getAllItems();
+    }
+
+
+
+     //Delete
+
+    @DeleteMapping("/item")
+    public Items deleteItem(@RequestBody Item item)
+    {
+        newItemDAO.deleteItem(item.getId());
+
+        return newItemDAO.getAllItems();
+    }
+
+
+}
--- sept-question-2/src/main/java/com/example/septquestion2/dao/ItemDAO.java
+++ sept-question-2/src/main/java/com/example/septquestion2/dao/ItemDAO.java
@@ -3,6 +3,10 @@ package com.example.septquestion2.dao;
 import com.example.septquestion2.model.Item;
 import org.springframework.stereotype.Repository;
 import com.example.septquestion2.model.Items;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Iterator;
 
 @Repository
 public class ItemDAO {
@@ -18,7 +22,39 @@ public class ItemDAO {
         return list;
     }
 
+    public Item getItemID(String id){
+        return list.getItemList().stream().filter(item -> id.equals(item.getId())).findAny().orElseThrow(null);
+    }
+
     public void addItem(Item item) {
         list.getItemList().add(item);
+    }
+
+    public Item updateItems(String id, String name, String desc, double price) {
+        Item holder = new Item();
+        for (int x = 0; x < list.getItemList().size(); x++){
+            Item item = list.getItemList().get(x);
+            if (item.getId().equals(id)){
+                item.setId(id);
+                item.setName(name);
+                item.setDesc(desc);
+                item.setPrice(price);
+                break;
+            }
+            else {
+                holder = null;
+            }
+        }
+        return holder;
+    }
+
+    public void deleteItem(String id) {
+        for (int x = 0; x < list.getItemList().size(); x++){
+            Item item = list.getItemList().get(x);
+            if (item.getId().equals(id)){
+                list.getItemList().remove(item);
+                break;
+            }
+        }
     }
 }
